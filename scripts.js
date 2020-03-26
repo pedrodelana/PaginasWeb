54bcc44d663b55de6212db135d2915c74f1b6627
Pedro Lana <pedrolana@hotmail.com>
2020-03-26 11:53:31 -0300

    Subindo arquivos e criando projeto

---
+++ scripts.js
@@ -0,0 +1,12 @@
+const modalOverlay = document.querySelector('.modal-overlay')
+const cards = document.querySelectorAll('.card')
+
+const teste = document.querySelectorAll('hidden')
+
+for(let card of cards){
+    const videoId = card.getAttribute('id')
+    card.addEventListener('click', function(){
+        modalOverlay.classList.add('active')
+        modalOverlay.querySelector("iframe").src = `https://www.youtube.com/embed/${videoId}`
+    })
+}
